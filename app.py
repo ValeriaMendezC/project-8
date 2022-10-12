ea8c76747ee86ebfc673a431378009573139a163
ValeriaMendezC <110490590+ValeriaMendezC@users.noreply.github.com>
2022-10-12 02:44:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pickle as pkl
 
 data=pd.read_csv("https://raw.githubusercontent.com/labeconometria/MLxE/main/proyectos1er/dataset_2.csv")
 st.title('Proyecto 8') 
-
-st.subheader('Descripción de variables')
+st.write("A partir de la siguiente base de datos se analiza si la persona clasificaría como un cliente existente o un cliente retirado')
 st.dataframe(data)
+st.subheader('Descripción de variables')
+
