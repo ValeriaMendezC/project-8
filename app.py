2ff8968f4e288e97bb9021cb9567646028af3a1d
ValeriaMendezC <110490590+ValeriaMendezC@users.noreply.github.com>
2022-10-12 02:26:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd 
+import numpy as np 
+import pickle as pkl
 
+data=pd.read_csv("https://raw.githubusercontent.com/labeconometria/MLxE/main/proyectos1er/dataset_2.csv")
 st.title('hola')
 
-
+st.image("./img/img1.png")
