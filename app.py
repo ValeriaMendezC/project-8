6e8e4d0d058b8f28feb99517d958b506df538092
ValeriaMendezC <110490590+ValeriaMendezC@users.noreply.github.com>
2022-10-12 03:05:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.subheader('Descripción de variables')
 col1,col2=st.columns(2)
 with col1:
   st.write("""0 CLIENTNUM: "Número de cliente"  """)
-  st.write(""" 1   Attrition_Flag    """)
+  st.write(""" 1   Attrition_Flag: "Variable de actividad del cliente""")
   st.write( """2   Customer_Age      """)                                                                                                                    
   st.write( """ 3   Gender          """)                                                                                                                      
   st.write( """ 4   Dependent_count  """)                                                                                                                     
@@ -27,7 +27,7 @@ with col1:
   
 with col2: 
   st.write("""Identificador único del cliente titular de la cuenta""")
-  st.write("""Variable de actividad del cliente - si la cuenta está cerrada, entonces 1, si no 0 """) 
+  st.write(""" Si la cuenta está cerrada, entonces 1, si no 0 """) 
   st.write( """Edad del cliente en años""")
   st.write( """M=Hombre, F=Mujer """)
   st.write( """Número de personas a cargo  """) 
