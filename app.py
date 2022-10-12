6f7b8bae63fdab3c759beccc40310c1787675217
ValeriaMendezC <110490590+ValeriaMendezC@users.noreply.github.com>
2022-10-12 03:03:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,30 @@ st.subheader('Descripción de variables')
 
 col1,col2=st.columns(2)
 with col1:
-  st.write("""0 CLIENTNUM: "Número de cliente"  """)
+  st.write("""0 CLIENTNUM: "Número de cliente"  """)}
+  st.write(""" 1   Attrition_Flag    """)
+  st.write( """2   Customer_Age      """)                                                                                                                    
+  st.write( """ 3   Gender          """)                                                                                                                      
+  st.write( """ 4   Dependent_count  """)                                                                                                                     
+  st.write( """ 5   Education_Level  """)    
+  st.write( """ 6   Marital_Status: "Estado civil"   """)
+  st.write( """ 7   Income_Category """) 
+  st.write( """ 8   Card_Category    """)           
+  st.write( """ 9   Months_on_book   """)                                                                                                                     
+  st.write( """ 10  Total_Relationship_Count     """)                                                                                                         
+  st.write( """ 11  Months_Inactive_12_mon """)                                                                                                               
+
   
 with col2: 
-  st.write("""Identificador único del cliente titular de la cuenta""")   
+  st.write("""Identificador único del cliente titular de la cuenta""")
+  st.write("""Variable de actividad del cliente - si la cuenta está cerrada, entonces 1, si no 0 """) 
+  st.write( """Edad del cliente en años""")
+  st.write( """M=Hombre, F=Mujer """)
+  st.write( """Número de personas a cargo  """) 
+  st.write( """Nivel educativo del titular de la cuenta """)
+  st.write( """ Casado, Soltero, Divorciado, Desconocido  """)
+  st.write( """Categoría de ingresos anuales del titular de la cuenta   """) 
+  st.write( """Tipo de tarjeta (Azul, Plata, Oro, Platino)  """) 
+  st.write( """Duración de la relación con el banco en meses """)
+  st.write( """Número total de productos en poder del cliente  """) 
+  st.write( """Número de meses de inactividad en los últimos 12 meses """) 
